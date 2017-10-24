22e7c68209acee517d390d8fec83ebf60b546c91
sergeyiandronov <sergeyiandronov@gmail.com>
2017-10-24 13:30:12 +0300

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -7,8 +7,26 @@ int Row;
 int Column;
 float **Matrix;
 };
-mtrx sum(float **matrix1, float **matrix2){
-     
+mtrx InitZero(int columns,int rows){
+mtrx result;
+float **matrix;
+
+     matrix = new float *[ rows];
+for(  int i = 0; i < rows; ++i ) {
+    matrix[ i ] = new float[ columns];
+for( int j = 0; j < columns; ++j ) {
+        matrix[ i ][ j ] = 0.0f;
+    }
+}
+result.Succes=true;result.Matrix=matrix;result.Column=columns;result.Row=rows;
+return result;
+	
+}
+mtrx sum(mtrx Mat){
+     mtrx result;
+     result=InitZero(Mat.Column,Mat.Row);
+     result.Matrix[0][0]=1;
+     return result;
 }
 bool getMatrix(float **matrix,int ncolumns,int nrows){
 
@@ -53,6 +71,7 @@ for( int j = 0; j < columns; ++j ) {
     }
 }result.Succes=getMatrix(matrix,columns,rows);result.Row=rows;result.Column=columns;result.Matrix=matrix;return result;}result.Succes=false;return result;}
 int main(){
+	mtrx Mat3sign;
 	 
         mtrx Mat1sign;
 mtrx Mat2sign;
@@ -63,22 +82,23 @@ getline(cin, strop);
 istringstream streamop(strop);
 streamop>>op;
 if(Mat1sign.Succes){switch(op){
-case ‘T’:
+case 'T':
     break;
-case ‘R’:
+case 'R':
     break;
-default:if((op!=‘+’)&&(op!=‘-‘)&&(op!=‘*’)){}
+default:if((op!='+')&&(op!='-')&&(op!='*')){}
     break;
 }}
 Mat2sign=getfullMatrix(); 
 
   if(Mat2sign.Succes){
           switch(op){
-case ‘+’:
+case '+':
+Mat3sign=sum(Mat2sign);
 break;
-case ‘*’:
+case '*':
 break;
-case ‘-‘:
+case '-':
 break;
 
 }
@@ -86,7 +106,9 @@ break;
 
 }
         
-
+if(Mat3sign.Succes){
+	coutMatrix(Mat3sign.Matrix,Mat3sign.Column,Mat3sign.Row);
+}
 
 
 
